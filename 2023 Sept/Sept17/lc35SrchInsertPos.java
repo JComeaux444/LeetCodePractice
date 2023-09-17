daa32d29f6fdc6f65a66e8a021e072b966150a86
JComeaux444 <129115698+JComeaux444@users.noreply.github.com>
2023-09-17 16:48:52 -0700

    Update lc35SrchInsertPos.java

--- 2023 Sept/Sept17/lc35SrchInsertPos.java
+++ 2023 Sept/Sept17/lc35SrchInsertPos.java
@@ -3,16 +3,22 @@ package Sept17;
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int output = 0;
+        // if target is less than or is equal to first index return 0
         if (target < nums[0] || target == nums[0]){
             return output;
         }
+        // loop as long as index not found
         for (int i = 0; i < nums.length-1; i++) {
+            // return index i if target matches
             if (target == nums[i]) {
                 return i;
             }
+            // return index i+1 if target matches 
+            // makes sure next case doesn't mess up
             if (target == nums[i+1]) {
                 return i+1;
             }
+            // if target can fit between the next 2 indexes, return i + 1
             if (target > nums[i] && target < nums[i+1]) {
                 return i+1;
             }
@@ -21,6 +27,7 @@ class Solution {
 
         }      
 
+        // everything else failed so it must fit at the end
         return nums.length;  
         
     }
